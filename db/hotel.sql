d3ca7dd2d46639d02c23ac66a324107b09b6e48b
saldm04 <s.dimartino25@studenti.unisa.it>
2024-05-04 15:59:09 +0200

    Aggiunta DataBase

---
+++ db/hotel.sql
@@ -0,0 +1,194 @@
+CREATE DATABASE  IF NOT EXISTS `hotel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hotel`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hotel
+-- ------------------------------------------------------
+-- Server version	8.3.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `camera`
+--
+
+DROP TABLE IF EXISTS `camera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `camera` (
+  `numero` int NOT NULL,
+  `numeroMaxOspiti` int NOT NULL,
+  `quadratura` int NOT NULL,
+  `costo` int NOT NULL,
+  `tipo` enum('standard','luxury','deluxe','exclusive') NOT NULL,
+  `descrizione` varchar(250) NOT NULL,
+  `img1` mediumblob,
+  `img2` mediumblob,
+  `img3` mediumblob,
+  `img4` mediumblob,
+  PRIMARY KEY (`numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `camera`
+--
+
+LOCK TABLES `camera` WRITE;
+/*!40000 ALTER TABLE `camera` DISABLE KEYS */;
+/*!40000 ALTER TABLE `camera` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `datecamera`
+--
+
+DROP TABLE IF EXISTS `datecamera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `datecamera` (
+  `codDateCamera` int NOT NULL AUTO_INCREMENT,
+  `dataInizio` date NOT NULL,
+  `dataFine` date NOT NULL,
+  `camera` int NOT NULL,
+  PRIMARY KEY (`codDateCamera`),
+  KEY `camera` (`camera`),
+  CONSTRAINT `datecamera_ibfk_1` FOREIGN KEY (`camera`) REFERENCES `camera` (`numero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `datecamera`
+--
+
+LOCK TABLES `datecamera` WRITE;
+/*!40000 ALTER TABLE `datecamera` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datecamera` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prenotazioni`
+--
+
+DROP TABLE IF EXISTS `prenotazioni`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prenotazioni` (
+  `idPrenotazione` int NOT NULL AUTO_INCREMENT,
+  `dataPrenotazione` datetime NOT NULL,
+  `importo` int NOT NULL,
+  `ordine` mediumblob NOT NULL,
+  `utente` varchar(40) NOT NULL,
+  PRIMARY KEY (`idPrenotazione`),
+  KEY `utente` (`utente`),
+  CONSTRAINT `prenotazioni_ibfk_1` FOREIGN KEY (`utente`) REFERENCES `utente` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prenotazioni`
+--
+
+LOCK TABLES `prenotazioni` WRITE;
+/*!40000 ALTER TABLE `prenotazioni` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prenotazioni` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `recensione`
+--
+
+DROP TABLE IF EXISTS `recensione`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `recensione` (
+  `codRecensione` int NOT NULL AUTO_INCREMENT,
+  `descrizione` varchar(300) DEFAULT NULL,
+  `voto` enum('1','2','3','4','5') NOT NULL,
+  `utente` varchar(40) NOT NULL,
+  PRIMARY KEY (`codRecensione`),
+  KEY `recensione_ibfk_1` (`utente`),
+  CONSTRAINT `recensione_ibfk_1` FOREIGN KEY (`utente`) REFERENCES `utente` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `recensione`
+--
+
+LOCK TABLES `recensione` WRITE;
+/*!40000 ALTER TABLE `recensione` DISABLE KEYS */;
+/*!40000 ALTER TABLE `recensione` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servizio`
+--
+
+DROP TABLE IF EXISTS `servizio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servizio` (
+  `nome` varchar(30) NOT NULL,
+  `descrizione` varchar(50) NOT NULL,
+  `costo` int NOT NULL,
+  PRIMARY KEY (`nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servizio`
+--
+
+LOCK TABLES `servizio` WRITE;
+/*!40000 ALTER TABLE `servizio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servizio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utente`
+--
+
+DROP TABLE IF EXISTS `utente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utente` (
+  `email` varchar(40) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `cognome` varchar(30) NOT NULL,
+  `nazionalità` varchar(30) NOT NULL,
+  `dataDiNascita` date NOT NULL,
+  `password` varchar(512) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utente`
+--
+
+LOCK TABLES `utente` WRITE;
+/*!40000 ALTER TABLE `utente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `utente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-04 15:57:02
